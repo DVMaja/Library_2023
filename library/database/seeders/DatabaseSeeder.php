9039184da1e75e24041fd1d33922879df3139671
DVMaja <majadreilinger@gmail.com>
2023-09-12 15:23:52 +0200

    adatbázis feltöltése random adatokal

--- library/database/seeders/DatabaseSeeder.php
+++ library/database/seeders/DatabaseSeeder.php
@@ -12,7 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(10)->create(); //fontos a sorrend
+         \App\Models\Book::factory(10)->create(); //ha roszz a sorrend probléma lesz
+         \App\Models\Copy::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
