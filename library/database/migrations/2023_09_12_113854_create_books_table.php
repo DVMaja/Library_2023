f07960791b3bdac1b5f3a36ad805f77289fb5b67
DVMaja <majadreilinger@gmail.com>
2023-09-12 14:00:40 +0200

    adatbázis szerkezete készen van

--- library/database/migrations/2023_09_12_113854_create_books_table.php
+++ library/database/migrations/2023_09_12_113854_create_books_table.php
@@ -11,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id(); //auto increment, primarykey bigint tipusú
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('books', function (Blueprint $table) {
+            $table->id('book_id');
+            $table->string('author', 32);
+            $table->string('title', 150);
+            $table->integer('pieces')->default(50);
+            
             $table->timestamps();
         });
     }
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('books');
     }
 };
--- library/database/migrations/2023_09_12_113946_create_copies_table.php
+++ library/database/migrations/2023_09_12_113946_create_copies_table.php
@@ -11,13 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id(); //auto increment, primarykey bigint tipusú
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('copies', function (Blueprint $table) {
+            $table->id('copy_id');
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->foreignId('book_id')->references('book_id')->on('books');
             $table->timestamps();
         });
     }
@@ -27,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('copies');
     }
 };
